088b853a6dcf7492933f6da98c92fd5ab8806d81
chuma-beep <wisboynelson123@gmail.com>
2024-05-03 18:54:33 +0100

    adjusted footer b-g

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,8 @@
-import { Button } from "./ui/button";
-
-
 
 export default function Footer() {
   return (
     <>
-      <footer className="bg-white dark:bg-gray-900">
+      <footer className="bg-white">
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
